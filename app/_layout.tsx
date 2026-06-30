09510ba3f25e9d9fde1f758a47894dbe87b1edf2
назрОС <102623319+nazrOS-softhard@users.noreply.github.com>
2026-06-30 07:44:46 +0300

    Add files via upload

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,11 @@
 import { Stack } from 'expo-router'
 import { StatusBar } from 'expo-status-bar'
 import { Colors } from '@/constants/Colors'
+import { ErrorBoundary } from '@/components/ErrorBoundary'
 
 export default function RootLayout() {
   return (
-    <>
+    <ErrorBoundary>
       <StatusBar style="light" backgroundColor={Colors.bg} />
       <Stack
         screenOptions={{
@@ -12,6 +13,6 @@ export default function RootLayout() {
           contentStyle: { backgroundColor: Colors.bg },
         }}
       />
-    </>
+    </ErrorBoundary>
   )
 }
